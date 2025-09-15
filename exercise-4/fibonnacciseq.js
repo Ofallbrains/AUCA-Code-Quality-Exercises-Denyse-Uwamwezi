16187ee7f1fd99803c53e879226636883123bf6a
Rebakure🔭 <165783014+KevinRebakure@users.noreply.github.com>
2025-09-15 07:18:26 -0700

    review fibonnacciseq.js

--- exercise-4/fibonnacciseq.js
+++ exercise-4/fibonnacciseq.js
@@ -1,7 +1,10 @@
 function fibonacci(n) {
   const sequence = [0, 1];
-  while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= n) {
-    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+  // Also leave a comment her
+  const sumOfTheLast2PreviousNumbers = sequence[sequence.length - 1] + sequence[sequence.length - 2]
+  
+  while (sumOfTheLast2PreviousNumbers <= n) {
+    sequence.push(sumOfTheLast2PreviousNumbers);
   }
   return sequence.filter(num => num <= n);
 }
