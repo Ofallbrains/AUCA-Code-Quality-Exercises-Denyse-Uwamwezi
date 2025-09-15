e3fc91e5996ef344fd48a5500ea37d0e34204ba3
Rebakure🔭 <165783014+KevinRebakure@users.noreply.github.com>
2025-09-15 07:14:56 -0700

    review decoder.js

--- exercise-4/decoder.js
+++ exercise-4/decoder.js
@@ -1,13 +1,12 @@
-function solution(roman) {
+function romanDecoder(roman) {
     const value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
 
   
   return roman.split('')
               .reduce((acc, cur, i, arr) => {
+                  // This can be tricky to understand. You can leave a comment here for clarity.
                 return (i !== arr.length-1 && value[cur] < value[arr[i+1]])
                   ? acc - value[cur]
                   : acc + value[cur]
               }, 0)
 }
-
-console.log(solution("MDCLXVI"));
